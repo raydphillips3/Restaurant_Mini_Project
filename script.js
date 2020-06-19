38fb1cb87ea53bd4c61d29f9f6afd6b362ff8a5d
Ray Phillips <raydphillips3@gmail.com>
2020-06-19 11:27:47 -0500

    new push

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ app.use(express.json());
 // Routes
 // =============================================================
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
